be0a0bba24a9b36b616dc766d7e11c9f15b6f8da
Selim Altınkaynak <selim@hipmedya.com>
2026-08-13 00:20:13 +0300

    Add French Projects metadata

--- app/fr/projets/page.tsx
+++ app/fr/projets/page.tsx
@@ -1,4 +1,9 @@
 import V2WorkListing from '@/components/V2WorkListing';
+import { listingMetadata } from '@/lib/seo';
+
+export function generateMetadata() {
+  return listingMetadata('work', 'fr');
+}
 
 export default function FrenchWorkPage() {
   return <V2WorkListing lang="fr" />;
