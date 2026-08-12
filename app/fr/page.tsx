c9b96363bb309a71b605b64e2821e5e66be929ac
Selim Altınkaynak <selim@hipmedya.com>
2026-08-12 21:22:27 +0300

    Use shared site shell on French home

--- app/fr/page.tsx
+++ app/fr/page.tsx
@@ -1,5 +1,13 @@
-import LegacySite from '@/components/LegacySite';
+import LegacyHomeContent from '@/components/LegacyHomeContent';
+import SiteFooter from '@/components/SiteFooter';
+import SiteHeader from '@/components/SiteHeader';
 
 export default function FrenchPage() {
-  return <LegacySite lang="fr" />;
+  return (
+    <>
+      <SiteHeader active="home" lang="fr" homePage />
+      <LegacyHomeContent lang="fr" />
+      <SiteFooter lang="fr" />
+    </>
+  );
 }
